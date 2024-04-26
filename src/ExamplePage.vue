5859d1f04d7e4a7f954ad047e9a3a25820c4fc4f
Konrad Kubczyk <konrad@kubczyk.com>
2024-04-26 12:34:52 +0200

    feat: add and configure Vue Router

---
+++ src/ExamplePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>
+    Example page
+  </h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid, assumenda blanditiis corporis
+    culpa eligendi eos esse est excepturi explicabo facere inventore iste iusto magnam magni nisi, numquam obcaecati
+    officia perferendis quia recusandae rem, repudiandae similique sint vel voluptatum? A ad at autem, consequuntur
+    cumque debitis dolores enim id impedit inventore iste labore maiores nemo neque nesciunt nisi nulla perspiciatis
+    quaerat quasi quibusdam quidem quo repellat repudiandae saepe sequi! Accusamus blanditiis, dignissimos dolorem
+    doloribus eos esse inventore magni mollitia odit optio quae quos ratione, vel. Asperiores at aut beatae consequatur
+    excepturi facere facilis ipsam ipsum laboriosam optio quae quasi reprehenderit tempore veritatis vitae, voluptatem
+    voluptates! Dolor hic inventore ipsam itaque! Aliquid asperiores dicta dolor dolores doloribus eos est et excepturi
+    fugit illo ipsa iste necessitatibus numquam placeat, quam qui rem voluptate voluptatum. Accusamus adipisci
+    architecto autem consequatur consequuntur cumque deserunt dicta dolorem dolores eum eveniet excepturi exercitationem
+    fugit harum hic in labore libero magni neque nesciunt odit optio placeat provident quae quis quos reiciendis
+    repellat sit soluta tempora ullam, unde vel vero. Aliquid beatae consectetur consequuntur dignissimos distinctio
+    dolorem dolores dolorum ex ipsam iure laboriosam minus necessitatibus nulla perspiciatis quas quisquam recusandae
+    repellendus rerum sapiente tempora, ullam voluptates voluptatibus.
+  </p>
+</template>
+
+<style scoped>
+
+</style>
