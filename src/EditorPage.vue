ea2a34a1f0ce8eecd3ea4b1ee878dfd8c0946306
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-04 01:32:41 +0200

    fix: minor menu layout tweaks

--- src/EditorPage.vue
+++ src/EditorPage.vue
@@ -71,14 +71,28 @@ window.addEventListener('resize', () => {
         <input type="text" placeholder="Note title" class="input w-full max-w-xs" :value="title" />
       </div>
       <div class="flex-none gap-2">
-        <button class="btn" onclick="ocr_modal.showModal()">OCR</button>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-square">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                 class="inline-block w-5 h-5 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+            </svg>
+          </div>
+          <ul tabindex="0" class="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <button onclick="ocr_modal.showModal()">OCR</button>
+            </li>
+            <!-- TODO: Other extra functionalities may be launched from here -->
+          </ul>
+        </div>
         <div class="dropdown dropdown-end">
           <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
             <div class="w-8 rounded-full">
               <img alt="Profile icon" src="./assets/img/default-profile-icon.svg" />
             </div>
           </div>
-          <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+          <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
             <li><a>Profile</a></li>
             <li><a>Settings</a></li>
             <li><a>Logout</a></li>
@@ -107,7 +121,6 @@ window.addEventListener('resize', () => {
           <div class="preview" v-html="preview"></div>
         </div>
       </div>
-
     </div>
     <div class="btm-nav sticky" v-if="isMobile">
       <button
