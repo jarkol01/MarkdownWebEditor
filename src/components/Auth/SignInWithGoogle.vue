f0b7d88ea3cc908940fee6c04ee110fd888ab2c4
Jarosław Kołdun <jkoldun@gmail.com>
2024-05-05 10:31:59 +0200

    Restyled AuthDropdown

--- src/components/Auth/SignInWithGoogle.vue
+++ src/components/Auth/SignInWithGoogle.vue
@@ -17,7 +17,7 @@ const signInWithGoogle = () => {
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex items-center justify-center m-2">
     <button class="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" @click="signInWithGoogle">
         <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
         <span>Login with Google</span>
