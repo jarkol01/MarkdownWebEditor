a2abc793cfa604c5f8e0d8be04f722c3554337d2
Konrad Kubczyk <konrad@kubczyk.com>
2024-05-05 14:35:33 +0200

    feat: navigation guard and redirections

--- src/router.js
+++ src/router.js
@@ -1,19 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/HomePage.vue'
-import ExamplePage from '@/ExamplePage.vue'
 import EditorPage from '@/EditorPage.vue'
+import RegistrationPage from '@/RegistrationPage.vue'
+import { getCurrentUser } from '@/firebase.js'
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/example', component: ExamplePage },
-  { path: '/editor', component: EditorPage },
-  { path: '/editor/:noteId', component: EditorPage }
+  {
+    path: '/',
+    redirect: '/editor'
+  },
+  {
+    path: '/editor',
+    component: EditorPage
+  },
+  {
+    path: '/editor/:noteId',
+    component: EditorPage
+  },
+  {
+    path: '/register',
+    component: RegistrationPage,
+    meta: {
+      requiresGuest: true
+    }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// Navigation guard
+router.beforeEach(async (to, from, next) => {
+  const requiresGuest = to.matched.some(record => record.meta.requiresGuest)
+  const currentUser = await getCurrentUser() // Check if the user is authenticated
+
+  if (requiresGuest && currentUser) {
+    next('/') // If the user is authenticated and the route requires guest, redirect to '/'
+  } else {
+    next() // Otherwise, proceed to the route
+  }
 })
 
 export default router
